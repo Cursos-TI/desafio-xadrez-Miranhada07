4d8034d6bc31e6cdc84da5bcb06529c8cebc348e
Octavio <octavioand@gmail.com>
2025-06-24 14:07:24 +0000

    Nova movimentacao

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    // Movimentação da Torre (5 casas para a direita) - usando FOR
+ 
     printf("Movimentação da Torre:\n");
     for (int i = 1; i <= 5; i++) {
         printf("Direita\n");
     }
     printf("\n");
 
-    // Movimentação do Bispo (5 casas na diagonal para cima e à direita) - usando WHILE
+    
     printf("Movimentação do Bispo:\n");
     int j = 1;
     while (j <= 5) {
@@ -17,15 +17,33 @@ int main() {
     }
     printf("\n");
 
-    // Movimentação da Rainha (8 casas para a esquerda) - usando DO-WHILE
+   
     printf("Movimentação da Rainha:\n");
     int k = 1;
     do {
         printf("Esquerda\n");
         k++;
     } while (k <= 8);
+    printf("\n");
+
+
+
+    printf("Movimentação do Cavalo:\n");
+
+   
+    for (int baixo = 1; baixo <= 2; baixo++) {
+        printf("Baixo\n");
+    }
+
+
+    int esquerda = 1;
+    while (esquerda <= 1) {
+        printf("Esquerda\n");
+        esquerda++;
+    }
 
     return 0;
 }
+
 
 
